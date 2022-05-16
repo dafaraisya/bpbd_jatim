0e83750c56ea949b534f09208efd4337501ecf1b
arisetiawan4601 <arisetiawan4601@gmail.com>
2022-05-16 13:57:14 +0700

    Change primary color

--- lib/themes/color_theme.dart
+++ lib/themes/color_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const ColorScheme colorScheme = ColorScheme.light(
-  primary: Color(0xFFEB1C24),
+  primary: Color(0xFFFE5E32),
   secondary: Color(0xFF8F959D),
   secondaryVariant: Color(0xFF485671),
   surface: Color(0xFF1E272E),
