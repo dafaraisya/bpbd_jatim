7a5ee7e870400d15c4c2e8e649a1b3d6df0ebd79
Dafa Raisya <dafaraisya.dr@gmail.com>
2022-05-16 14:50:23 +0700

    login done

---
+++ lib/components/button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  const Button({
+    Key? key,
+    this.text,
+    this.press,
+  }) : super(key: key);
+  final String? text;
+  final VoidCallback? press;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 56,
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).colorScheme.primary),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0)
+            )
+          )
+        ),
+        onPressed: press!,
+        child: Text(
+          text!,
+          style: const TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:bpbd_jatim/components/button.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatelessWidget {
@@ -9,94 +10,106 @@ class SignIn extends StatelessWidget {
     TextEditingController passwordController = TextEditingController();
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.topLeft,
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  'Login',
-                  style: Theme.of(context).textTheme.headline4?.copyWith(color: Theme.of(context).colorScheme.primary)
-                )),
-            Container(
-                alignment: Alignment.topLeft,
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  'Please sign in to continue',
-                  style: Theme.of(context).textTheme.headline6?.copyWith(color: Theme.of(context).colorScheme.surface)
-                )),
-            Container(
-                alignment: Alignment.topLeft,
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  'Email',
-                  style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Theme.of(context).colorScheme.secondary),
-                )),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
-                ),
-              ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "assets/images/sign_in_background.png",
+              alignment: Alignment.topRight,
             ),
-            Container(
-                alignment: Alignment.topLeft,
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  'Password',
-                  style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Theme.of(context).colorScheme.secondary),
-                )),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            Container(
-              alignment: Alignment.bottomRight,
-              child: TextButton(
-                onPressed: () {
-                  //forgot password screen
-                },
-                child: Text('Forgot Password?', style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Theme.of(context).colorScheme.secondary),),
-              ),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).colorScheme.primary)),
-                  child: const Text('Login'),
-                  onPressed: () {},
-                )
-            ),
-            Row(
-              children: <Widget>[
-                Text('Do not have an account yet?', style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Theme.of(context).colorScheme.secondary)),
-                TextButton(
-                  child: Text(
-                    'Register',
-                    style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Theme.of(context).colorScheme.primary)
-                  ),
-                  onPressed: () {
-                    //signup screen
-                  },
-                )
-              ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-            ),
-          ],
-        )
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Login',
+                      style: Theme.of(context).textTheme.headline4?.copyWith(color: Theme.of(context).colorScheme.primary)
+                    )),
+                Container(
+                    alignment: Alignment.topLeft,
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      'Please sign in to continue',
+                      style: Theme.of(context).textTheme.headline6?.copyWith(color: Theme.of(context).colorScheme.surface)
+                    )),
+                Container(
+                    alignment: Alignment.topLeft,
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      'Email',
+                      style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Theme.of(context).colorScheme.secondary),
+                    )),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      hintText: 'Enter email address'
+                    ),
+                  ),
+                ),
+                Container(
+                    alignment: Alignment.topLeft,
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      'Password',
+                      style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Theme.of(context).colorScheme.secondary),
+                    )),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      hintText: 'Enter password'
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.bottomRight,
+                  child: TextButton(
+                    onPressed: () {
+                      //forgot password screen
+                    },
+                    child: Text('Forgot Password?', style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Theme.of(context).colorScheme.secondary),),
+                  ),
+                ),
+                Container(
+                    height: 50,
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: Button(press: () => {}, text: 'Login',)
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('Do not have an account yet?', style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Theme.of(context).colorScheme.secondary)),
+                    TextButton(
+                      child: Text(
+                        'Register',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Theme.of(context).colorScheme.primary)
+                      ),
+                      onPressed: () {
+                        //signup screen
+                      },
+                    )
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+              ],
+            )
+          ),
+        ],
       )
     );
   }
