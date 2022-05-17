a6dbac2623fc29a864e8dd6ef11ceeeecb81cb86
bakarbsa <abubakar.bsa30@gmail.com>
2022-05-17 20:40:35 +0700

    replace text black color

--- lib/themes/text_theme.dart
+++ lib/themes/text_theme.dart
@@ -6,32 +6,14 @@ TextTheme textTheme = TextTheme(
     fontSize: 34,
     fontWeight: FontWeight.w500,
   ),
-  headline5: GoogleFonts.lato(
-    fontSize: 24,
-    fontWeight: FontWeight.w500
-  ),
-  headline6: GoogleFonts.lato(
-    fontSize: 20,
-    fontWeight: FontWeight.w500
-  ),
-  bodyText1: GoogleFonts.lato(
-    fontSize: 16,
-    fontWeight: FontWeight.w500
-  ),
-  bodyText2: GoogleFonts.lato(
-    fontSize: 14,
-    fontWeight: FontWeight.w500
-  ),
-  subtitle2: GoogleFonts.lato(
-    fontSize: 14,
-    fontWeight: FontWeight.w500
-  ),
-  caption: GoogleFonts.lato(
-    fontSize: 12,
-    fontWeight: FontWeight.w500
-  ),
-  button: GoogleFonts.lato(
-    fontSize: 20,
-    fontWeight: FontWeight.w500
-  ),
+  headline5: GoogleFonts.lato(fontSize: 24, fontWeight: FontWeight.w500),
+  headline6: GoogleFonts.lato(fontSize: 20, fontWeight: FontWeight.w500),
+  bodyText1: GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w500),
+  bodyText2: GoogleFonts.lato(fontSize: 14, fontWeight: FontWeight.w500),
+  subtitle2: GoogleFonts.lato(fontSize: 14, fontWeight: FontWeight.w500),
+  caption: GoogleFonts.lato(fontSize: 12, fontWeight: FontWeight.w500),
+  button: GoogleFonts.lato(fontSize: 20, fontWeight: FontWeight.w500),
+).apply(
+  displayColor: const Color(0xFF1E272E),
+  bodyColor: const Color(0xFF1E272E),
 );
