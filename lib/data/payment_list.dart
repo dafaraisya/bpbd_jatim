474e157eee33d2b48015688bbb3659be86a9b1f6
bakarbsa <abubakar.bsa30@gmail.com>
2022-05-25 09:16:51 +0700

    add light theme button & donation screen

--- lib/data/payment_list.dart
+++ lib/data/payment_list.dart
@@ -1,1 +1,17 @@
 enum PaymentList { linkAja, ovo, gopay, mandiri, bca, bri }
+
+getAsset(PaymentList paymentMethod) {
+  if (paymentMethod == PaymentList.linkAja) {
+    return 'linkaja.png';
+  } else if (paymentMethod == PaymentList.ovo) {
+    return 'ovo.png';
+  } else if (paymentMethod == PaymentList.gopay) {
+    return 'gopay.png';
+  } else if (paymentMethod == PaymentList.mandiri) {
+    return 'mandiri.png';
+  } else if (paymentMethod == PaymentList.bca) {
+    return 'bca.png';
+  } else if (paymentMethod == PaymentList.bri) {
+    return 'bri.png';
+  }
+}
--- lib/screens/user/donation/finalization.dart
+++ lib/screens/user/donation/finalization.dart
@@ -1,12 +1,39 @@
+import 'package:bpbd_jatim/components/button.dart';
 import 'package:flutter/material.dart';
 
 class Finalization extends StatelessWidget {
-  const Finalization({ Key? key }) : super(key: key);
+  const Finalization({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(height: 59),
+        Center(child: Image.asset('assets/images/donation_illustration.png')),
+        const SizedBox(height: 47),
+        Text(
+          'Transaksi telah berhasil !!!',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        const SizedBox(height: 40),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Button(
+            press: () {},
+            text: 'Kembali ke halaman utama',
+          ),
+        ),
+        const SizedBox(height: 8),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Button(
+            press: () {},
+            text: 'Upload bukti transfer',
+            isSecondary: true,
+          ),
+        ),
+      ],
     );
   }
 }
